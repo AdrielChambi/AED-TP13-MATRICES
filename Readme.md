7272293be59e255bc2a52be1e7b82130b0548d70
AdrielChambi <dchambi@fuerzas-armadas.mil.ar>
2015-09-14 14:46:49 -0300

    subo TP13 comienzo

---
+++ Readme.md
@@ -0,0 +1,33 @@
+﻿#AED-TP13-MATRICES
+***
+##Integrantes:
+* *Federico BUSTAMANTE*
+* *Augusto SCHMID*
+* *Martin AGUEL*
+* *Adriel CHAMBI*
+
+##Requisito:
+Leer los importes de las ventas del año pasado de N vendedores que hicieron por cada una de las M regiones
+Informar:
+
+1. Ventas total del año pasado.
+2. Ventas por region.
+3. Ventas por vendedor
+4. Vendedor con mas ventas.
+5. Region con mas ventas.
+
+##Ejemplo:
+Vendedor 1, Region 1: $250, Region 2: $300
+Vendedor 2, Region 1: $ 10, Region 2: $ 50
+
+1. Ventas Total: $250 + $300 + $10 + $50 = $610
+2. Ventas por region: Region 1: $260, Region 2: $350
+3. Ventas por vendedor: Vendedor 1: $550, Vendedor 2: $60
+4. Vendedor con mas ventas: Vendedor 1: $550
+5. Region con mas ventas: Region 2: $350
+
+##Pre-Condiciones:
+El usuario ingresa bien los valores numericos
+
+##Materia
+Algoritmo y Estructura de Datos.
---
+++ main.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdlib>
+#include <array>    // Se necesita activar C++11
+
+/* Trabajo Practico N13 - MATRICES
+
+REQUISITO:
+Leer los importes de las ventas del año pasado de N vendedores que hicieron por cada una de las M regiones
+Informar:
+- 1. Ventas total del año pasado.
+- 2. Ventas por region.
+- 3. Ventas por vendedor
+- 4. Vendedor con mas ventas.
+- 5. Region con mas ventas.
+
+EJEMPLO:
+Vendedor 1, Region 1: $250, Region 2: $300
+Vendedor 2, Region 1: $ 10, Region 2: $ 50
+
+- 1. Ventas Total: $250 + $300 + $10 + $50 = $610
+- 2. Ventas por region: Region 1: $260, Region 2: $350
+- 3. Ventas por vendedor: Vendedor 1: $550, Vendedor 2: $60
+- 4. Vendedor con mas ventas: Vendedor 1: $550
+- 5. Region con mas ventas: Region 2: $350
+
+PRE-CONDICIONES:
+El usuario ingresa bien los valores numericos
+
+MATERIA:
+Algoritmos y Estructura de Datos
+
+INTEGRANTES:
+Martin AGUEL
+Adriel CHAMBI
+Federico BUSTAMANTE
+Augusto SCHMID
+
+*/
+using namespace std;
+
+int main(int argc, char** argv) {
+	
+	
+	
+	return 0;
+}
